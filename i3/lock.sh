8212b0e7f2e3cd51498cf75e0ce9bea67fead795
Nick Gregory <computerfreak97@gmail.com>
2019-01-04 13:24:36 -0500

    i3status and i3lock changes

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,29 +1,38 @@
 #!/bin/sh
 
-B='#00000000'  # blank
-D='#ffffffcc'  # default
-T='#ffffffcc'  # text
-W='#880000bb'  # wrong
-V='#008800bb'  # verifying
+TMPBG='/tmp/screen.png'
+scrot ${TMPBG}
+convert ${TMPBG} -scale 5% -scale 2000% ${TMPBG}
 
-i3lock                \
---ringvercolor=$V     \
---ringwrongcolor=$W   \
-\
---insidecolor=$B      \
---ringcolor=$D        \
---linecolor=$B        \
---separatorcolor=$D   \
-\
---textcolor=$T        \
---timecolor=$T        \
---datecolor=$T        \
---keyhlcolor=$V       \
---bshlcolor=$W        \
-\
---screen 0            \
---blur 10             \
---clock               \
---indicator           \
---timestr="%H:%M:%S"  \
---datestr="%a, %d %b %Y" \
+B='00000000'  # blank
+V='008800bb'  # verifying
+W='880000bb'  # wrong
+
+D='00000088'  # default ring
+T='ffffffff'  # text
+
+i3lock                    \
+    -i ${TMPBG}           \
+    --ringvercolor=$V     \
+    --ringwrongcolor=$W   \
+    \
+    --insidecolor=$D      \
+    --ringcolor=$D        \
+    --linecolor=$B        \
+    --separatorcolor=$D   \
+    \
+    --verifcolor=$T       \
+    --wrongcolor=$T       \
+    --layoutcolor=$T      \
+    --timecolor=$T        \
+    --datecolor=$T        \
+    --keyhlcolor=$V       \
+    --bshlcolor=$W        \
+    \
+    --clock               \
+    --indicator           \
+    --timestr="%H:%M:%S"  \
+    --datestr="%a, %d %b %Y" \
+    \
+    --ignore-empty-password \
+    --show-failed-attempts
