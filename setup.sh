0eebc8ee492a21c99e0a6500c6fcec47a71c0aad
Nick Gregory <computerfreak97@gmail.com>
2021-07-14 19:37:28 -0400

    no more ctags; bump plugin versions

--- setup.sh
+++ setup.sh
@@ -20,7 +20,6 @@ function setup_path {
 
 # link everything out to home, moving existing files/dirs to {existing}.old
 setup_path "conkyrc" ".conkyrc"
-setup_path "ctags.conf" ".ctags"
 setup_path "radare2rc" ".radare2rc"
 setup_path "tmux.conf" ".tmux.conf"
 setup_path "vim" ".vim"
@@ -35,29 +34,37 @@ git config --global core.hookspath "${SCRIPTPATH}/git_hooks"
 git config --global core.excludesfile "${SCRIPTPATH}/gitignore"
 
 if hash apt 2>/dev/null; then
+    sudo apt update
+
     echo "Installing shell and other essentials"
-    sudo apt install -y zsh tmux
+    sudo apt install -y zsh tmux curl vim
+    echo "chsh:"
     chsh -s $(which zsh)
 
     echo "Installing build utils"
     sudo apt install -y build-essential autoconf pkg-config
 
-    echo "Building and installing ctags"
-    pushd /tmp
-    git clone https://github.com/universal-ctags/ctags
-    cd ctags
-    ./autogen.sh
-    ./configure
-    make -j $(nproc)
-    sudo make install
-    popd
-
-    rm -rf /tmp/ctags
-
     echo "Installing clang-format"
     sudo apt install -y clang-format
+
+    if [ "$(lsb_release -si)" == "Ubuntu" ]; then
+        echo "Installing go"
+        sudo add-apt-repository ppa:longsleep/golang-backports
+        sudo apt update
+        sudo apt install golang-go
+
+        echo "Installing docker"
+        sudo apt install -y apt-transport-https ca-certificates gnupg lsb-release
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+        echo \
+          "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+          $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io
+        sudo usermod -aG docker "${USER}"
+    else
+        echo "You'll need to manually install go and docker"
+    fi
 else
-    hash ctags 2>/dev/null || echo "You need to install ctags (https://github.com/universal-ctags/ctags)"
     hash zsh 2>/dev/null || echo "You need to install zsh"
     hash tmux 2>/dev/null || echo "You need to install tmux"
     hash clang-format 2>/dev/null || echo "You should install clang/clang-format"
